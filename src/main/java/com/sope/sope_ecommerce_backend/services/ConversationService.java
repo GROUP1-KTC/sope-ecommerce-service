0b8107c139780da8ea7161d5313b0cc5bce71447
Pham Van Phuc <phucpham.1803@gmail.com>
2025-09-08 15:50:30 +0700

    complete web socket for chat

--- src/main/java/com/sope/sope_ecommerce_backend/services/ConversationService.java
+++ src/main/java/com/sope/sope_ecommerce_backend/services/ConversationService.java
@@ -2,15 +2,12 @@ package com.sope.sope_ecommerce_backend.services;
 
 import com.sope.sope_ecommerce_backend.dto.request.ConversationCreateRequest;
 import com.sope.sope_ecommerce_backend.dto.response.ConversationResponse;
-import com.sope.sope_ecommerce_backend.entities.Conversation;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 public interface ConversationService {
     ConversationResponse createConversation (ConversationCreateRequest request);
     List<ConversationResponse> getConversationById(UUID conversationId);
     List<ConversationResponse> getUserConversations();
-//    void deleteConversation(UUID conversationId, UUID requesterId);
 }
