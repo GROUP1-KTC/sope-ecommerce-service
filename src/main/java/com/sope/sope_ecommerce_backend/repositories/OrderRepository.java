241ae7c2cae09919570fdd159cb238006c72591a
Pham Van Phuc <phucpham.1803@gmail.com>
2025-09-15 21:52:31 +0700

    update load product by suggestion

--- src/main/java/com/sope/sope_ecommerce_backend/repositories/OrderRepository.java
+++ src/main/java/com/sope/sope_ecommerce_backend/repositories/OrderRepository.java
@@ -42,7 +42,6 @@ public interface OrderRepository extends JpaRepository<Order, UUID> {
 
     Page<Order> findByShop_IdAndStatus(UUID shopId, OrderStatus status, Pageable pageable);
 
-    Optional<Order> findByOrderNumber(String orderNumber);
 
     List<Order> findByShop_Id(UUID shopId);
 }
